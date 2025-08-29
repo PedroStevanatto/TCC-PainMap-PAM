6bfac70de8cd1ab5d3f4cd03807c3987f9d442ca
VINICIUS VIANA ALVES <vinicius.alves202@etec.sp.gov.br>
2025-08-29 09:28:16 -0300

    Adicionando as paginas de alerta

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import FormPage from "./pages/FormPage"
+import AlertDTM from './pages/AlertDTM';
+import NaoDTM from './pages/NaoDTM';
 import { StyleSheet } from "react-native";
 
 const Stack = createStackNavigator();
@@ -11,6 +13,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="FormPage">
         <Stack.Screen name="FormPage" component={FormPage} options={{headerStyle: styles.header, headerTitleStyle: styles.title}} />
+        <Stack.Screen name="AlertDTM" component={AlertDTM} options={{headerStyle: styles.header, headerTitleStyle: styles.title}} />
+        <Stack.Screen name="NaoDTM" component={NaoDTM} options={{headerStyle: styles.header, headerTitleStyle: styles.title}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- pages/FormPage.js
+++ pages/FormPage.js
@@ -2,7 +2,33 @@ import React, { useState } from "react";
 import { View, Text, ScrollView, TouchableOpacity, StyleSheet } from "react-native";
 import { RadioButton } from "react-native-paper";
 
-export default function FormPage() {
+export function Form(respostas) {
+  const {
+    dorMandibula,
+    rigidezAoAcordar,
+    mastigar,
+    movimentarMandibula,
+    habitosMandibula,
+    outrasAtividades
+  } = respostas;
+
+  let pontuacaoDTM = 0;
+
+  if (dorMandibula !== "Não tive dor" && dorMandibula !== "") pontuacaoDTM++;
+  if (rigidezAoAcordar === "Sim") pontuacaoDTM++;
+  if (mastigar === "Sim") pontuacaoDTM++;
+  if (movimentarMandibula === "Sim") pontuacaoDTM++;
+  if (habitosMandibula === "Sim") pontuacaoDTM++;
+  if (outrasAtividades === "Sim") pontuacaoDTM++;
+
+  if (pontuacaoDTM >= 4) {
+    alert("Possível DTM detectada. Procure um profissional.");
+  } else {
+    alert("Fique tranquilo! Você provavelmente não possui DTM.");
+  }
+}
+
+export default function FormPage(navigation) {
   const [respostas, setRespostas] = useState({
     ansiedade: "",
     depressao: "",
@@ -18,130 +44,104 @@ export default function FormPage() {
     setRespostas({ ...respostas, [campo]: valor });
   }
 
-  function Form(respostas) {
-    const {
-      dorMandibula,
-      rigidezAoAcordar,
-      mastigar,
-      movimentarMandibula,
-      habitosMandibula,
-      outrasAtividades
-    } = respostas;
-
-    let pontuacaoDTM = 0;
-
-    if (dorMandibula !== "Não tive dor" && dorMandibula !== "") pontuacaoDTM++;
-    if (rigidezAoAcordar === "Sim") pontuacaoDTM++;
-    if (mastigar === "Sim") pontuacaoDTM++;
-    if (movimentarMandibula === "Sim") pontuacaoDTM++;
-    if (habitosMandibula === "Sim") pontuacaoDTM++;
-    if (outrasAtividades === "Sim") pontuacaoDTM++;
-
-    if (pontuacaoDTM >= 3) {
-      alert("Possível DTM detectada. Procure um profissional.");
-    } else {
-      alert("Fique tranquilo! Provavelmente você não possui DTM!");
-    }
-  }
-
   return (
     <ScrollView style={styles.container} contentContainerStyle={{ paddingBottom: 60 }}>
       <Text style={styles.title}>Questionário de triagem</Text>
-      
+
       <View style={styles.v} >
 
-      <Text style={styles.question}>Você tem ou já teve ansiedade?</Text>
-      <RadioButton.Group
-        onValueChange={(v) => handleChange("ansiedade", v)}
-        value={respostas.ansiedade}
-      >
-        <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
-        <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff"/>
-      </RadioButton.Group>
+        <Text style={styles.question}>Você tem ou já teve ansiedade?</Text>
+        <RadioButton.Group
+          onValueChange={(v) => handleChange("ansiedade", v)}
+          value={respostas.ansiedade}
+        >
+          <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
+          <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
+        </RadioButton.Group>
 
-      <Text style={styles.question}>Você tem ou já teve depressão?</Text>
-      <RadioButton.Group
-        onValueChange={(v) => handleChange("depressao", v)}
-        value={respostas.depressao}
-      >
-        <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
-        <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
-      </RadioButton.Group>
+        <Text style={styles.question}>Você tem ou já teve depressão?</Text>
+        <RadioButton.Group
+          onValueChange={(v) => handleChange("depressao", v)}
+          value={respostas.depressao}
+        >
+          <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
+          <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
+        </RadioButton.Group>
 
-      <Text style={styles.question}>
-        Nos últimos 30 dias, quanto tempo durou qualquer dor que você teve na mandíbula ou região temporal?
-      </Text>
-      <RadioButton.Group
-        onValueChange={(v) => handleChange("dorMandibula", v)}
-        value={respostas.dorMandibula}
-      >
-        <RadioButton.Item label="Não tive dor" value="Não tive dor" labelStyle={styles.item} color="#fff" />
-        <RadioButton.Item label="Dor aparecia e desaparecia" value="Dor aparecia e desaparecia" labelStyle={styles.item} color="#fff" />
-        <RadioButton.Item label="Dor estava sempre presente" value="Dor estava sempre presente" labelStyle={styles.item} color="#fff" />
-      </RadioButton.Group>
+        <Text style={styles.question}>
+          Nos últimos 30 dias, quanto tempo durou qualquer dor que você teve na mandíbula ou região temporal?
+        </Text>
+        <RadioButton.Group
+          onValueChange={(v) => handleChange("dorMandibula", v)}
+          value={respostas.dorMandibula}
+        >
+          <RadioButton.Item label="Não tive dor" value="Não tive dor" labelStyle={styles.item} color="#fff" />
+          <RadioButton.Item label="Dor aparecia e desaparecia" value="Dor aparecia e desaparecia" labelStyle={styles.item} color="#fff" />
+          <RadioButton.Item label="Dor estava sempre presente" value="Dor estava sempre presente" labelStyle={styles.item} color="#fff" />
+        </RadioButton.Group>
 
-      <Text style={styles.question}>
-        Nos últimos 30 dias, você teve dor ou rigidez na sua mandíbula ao acordar?
-      </Text>
-      <RadioButton.Group
-        onValueChange={(v) => handleChange("rigidezAoAcordar", v)}
-        value={respostas.rigidezAoAcordar}
-      >
-        <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
-        <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
-      </RadioButton.Group>
+        <Text style={styles.question}>
+          Nos últimos 30 dias, você teve dor ou rigidez na sua mandíbula ao acordar?
+        </Text>
+        <RadioButton.Group
+          onValueChange={(v) => handleChange("rigidezAoAcordar", v)}
+          value={respostas.rigidezAoAcordar}
+        >
+          <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
+          <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
+        </RadioButton.Group>
 
-      <Text style={styles.question}>
-        A) Mastigar alimentos duros ou consistentes mudou a dor?
-      </Text>
-      <RadioButton.Group
-        onValueChange={(v) => handleChange("mastigar", v)}
-        value={respostas.mastigar}
-      >
-        <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
-        <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
-      </RadioButton.Group>
+        <Text style={styles.question}>
+          A) Mastigar alimentos duros ou consistentes mudou a dor?
+        </Text>
+        <RadioButton.Group
+          onValueChange={(v) => handleChange("mastigar", v)}
+          value={respostas.mastigar}
+        >
+          <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
+          <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
+        </RadioButton.Group>
 
-      <Text style={styles.question}>
-        B) Abrir a boca ou movimentar a mandíbula mudou a dor?
-      </Text>
-      <RadioButton.Group
-        onValueChange={(v) => handleChange("movimentarMandibula", v)}
-        value={respostas.movimentarMandibula}
-      >
-        <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
-        <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
-      </RadioButton.Group>
+        <Text style={styles.question}>
+          B) Abrir a boca ou movimentar a mandíbula mudou a dor?
+        </Text>
+        <RadioButton.Group
+          onValueChange={(v) => handleChange("movimentarMandibula", v)}
+          value={respostas.movimentarMandibula}
+        >
+          <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
+          <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
+        </RadioButton.Group>
 
-      <Text style={styles.question}>
-        C) Hábitos como apertar ou ranger os dentes, ou mascar chiclete mudaram a dor?
-      </Text>
-      <RadioButton.Group
-        onValueChange={(v) => handleChange("habitosMandibula", v)}
-        value={respostas.habitosMandibula}
-      >
-        <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
-        <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
-      </RadioButton.Group>
+        <Text style={styles.question}>
+          C) Hábitos como apertar ou ranger os dentes, ou mascar chiclete mudaram a dor?
+        </Text>
+        <RadioButton.Group
+          onValueChange={(v) => handleChange("habitosMandibula", v)}
+          value={respostas.habitosMandibula}
+        >
+          <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
+          <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
+        </RadioButton.Group>
 
-      <Text style={styles.question}>
-        D) Outras atividades como falar, beijar ou bocejar mudaram a dor?
-      </Text>
-      <RadioButton.Group
-        onValueChange={(v) => handleChange("outrasAtividades", v)}
-        value={respostas.outrasAtividades}
-      >
-        <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
-        <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
-      </RadioButton.Group>
+        <Text style={styles.question}>
+          D) Outras atividades como falar, beijar ou bocejar mudaram a dor?
+        </Text>
+        <RadioButton.Group
+          onValueChange={(v) => handleChange("outrasAtividades", v)}
+          value={respostas.outrasAtividades}
+        >
+          <RadioButton.Item label="Sim" value="Sim" labelStyle={styles.item} color="#fff" />
+          <RadioButton.Item label="Não" value="Não" labelStyle={styles.item} color="#fff" />
+        </RadioButton.Group>
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => Form(respostas)}
-      >
-        <Text style={styles.buttonText}>ENVIAR</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => Form(respostas)}
+        >
+          <Text style={styles.buttonText}>ENVIAR</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 }
